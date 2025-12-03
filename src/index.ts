453782cc27836a98f611c4b5f2908616ad5a9f39
realm2100 <kur2100a@gmail.com>
2025-12-03 20:07:24 +0900

    add auth check to router

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/index.css";
 import { router } from "./router/index";
+import { isAuthed } from "./utils/auth/store";
+
+router.beforeEach((to, from, next) => {
+  if (to.path !== "/login" && !isAuthed) {
+    next("/login");
+  } else {
+    next();
+  }
+});
 
 createApp(App).use(router).mount("#app");
---
+++ src/utils/auth/store.ts
@@ -0,0 +1,3 @@
+let isAuthed = false;
+
+export { isAuthed };

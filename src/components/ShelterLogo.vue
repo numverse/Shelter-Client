0a9ba65e4ca8066e209b00a82681c7ecb68d7306
realm2100 <kur2100a@gmail.com>
2025-12-17 15:22:15 +0900

    add Shelter Logo

---
+++ src/components/ShelterLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-row items-start mb-12 -mt-12">
+    <img
+      src="../assets/shelter_logo.png"
+      alt="Shelter Logo"
+      class="w-15 h-15"
+    >
+    <p class="ml-2 text-6xl font-bold text-text-primary">
+      Shelter
+      <span class="text-accent -ml-4">
+        .
+      </span>
+    </p>
+  </div>
+</template>

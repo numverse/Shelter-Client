69920ac52c6ccfc90800404c27bf77cc977e9dd5
realm2100 <kur2100a@gmail.com>
2025-12-19 21:51:20 +0900

    move auth check to index, add loading circle

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 import { router } from "./router";
-import { checkAuthed } from "./utils/auth/store";
-
-await checkAuthed();
 
 createApp(App).use(router).mount("#app");
--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,20 +1,33 @@
 <script setup lang="ts">
-import { ref, computed, nextTick } from "vue";
+import { ref, computed, nextTick, onMounted } from "vue";
 import TextInputBox from "../components/TextInputBox.vue";
 import ShelterLogo from "../components/ShelterLogo.vue";
 import { login } from "../utils/api/auth/login";
 import { register } from "../utils/api/auth/register";
 import { i18n } from "../utils/i18n";
 import enUS from "../locales/en-US.json";
-import { setAuthed } from "../utils/auth/store";
+import { checkAuthed, setAuthed } from "../utils/auth/store";
 import { useRouter } from "vue-router";
+import LoadingCircle from "../components/LoadingCircle.vue";
 const router = useRouter();
 
 const authForm = ref<HTMLFormElement | null>(null);
 
 const isLogin = ref(true);
 const isLoading = ref(false);
+const isCheckingAuth = ref(true);
 const authFormIsValid = ref(false);
+
+onMounted(async () => {
+  const authed = await checkAuthed();
+  setTimeout(() => {
+    if (authed) {
+      return router.replace("/chat");
+    } else {
+      isCheckingAuth.value = false;
+    }
+  }, 300);
+});
 
 const checkAuthFormValidity = async () => {
   await nextTick();
@@ -72,87 +85,106 @@ function toggleMode() {
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center h-full bg-bg-tertiary">
+  <div class="flex flex-col gap-6 justify-center items-center h-full bg-bg-tertiary">
     <ShelterLogo />
-    <div class="relative bg-bg-secondary rounded-2xl flex flex-col items-center justify-center z-10 w-fit py-8 px-12">
-      <h1 class="text-4xl font-bold mb-6">
-        {{ isLogin ? i18n("login") : i18n("register") }}
-      </h1>
-      <form
-        ref="authForm"
-        class="space-y-4 w-md"
-        @submit.prevent="onSubmit"
-        @input="checkAuthFormValidity"
-        @change="checkAuthFormValidity"
+    <Transition
+      name="fade"
+      mode="out-in"
+      appear
+    >
+      <div
+        :key="isCheckingAuth ? 'loading' : 'form'"
+        class="flex items-center justify-center"
       >
-        <TextInputBox
-          v-if="!isLogin"
-          id="username"
-          v-model="username"
-          :label="i18n('username')"
-          type="text"
-          :placeholder="i18n('username_placeholder')"
-          :minlength="2"
-          :maxlength="32"
-          :required="true"
+        <LoadingCircle
+          v-if="isCheckingAuth"
+          class="w-16 h-16 text-accent animate-spin"
         />
-        <TextInputBox
-          id="email"
-          v-model="email"
-          :label="i18n('email')"
-          type="email"
-          :placeholder="i18n('email_placeholder')"
-          :required="true"
-        />
-        <TextInputBox
-          id="password"
-          v-model="password"
-          :label="i18n('password')"
-          type="password"
-          :placeholder="i18n('password_placeholder')"
-          :minlength="6"
-          :maxlength="64"
-          :required="true"
-        />
-        <button
-          type="submit"
-          :disabled="!canSubmit"
-          class="w-full bg-accent text-text-primary text-xl font-bold py-2 rounded-sm hover:bg-accent/80 disabled:opacity-50 hover:cursor-pointer disabled:hover:cursor-not-allowed transition-all duration-200"
+        <div
+          v-if="!isCheckingAuth"
+          class="relative bg-bg-secondary rounded-2xl flex flex-col items-center justify-center z-10 w-fit py-8 px-12"
         >
-          {{ isLogin ? isLoading ? i18n("login_loading") : i18n("login") : isLoading ? i18n("register_loading") : i18n("register") }}
-        </button>
-
-        <p
-          v-if="errorMessage"
-          class="text-red-500 -mt-2 text-sm"
-        >
-          {{ errorMessage }}
-        </p>
-      </form>
-
-      <p class="mt-4 text-sm text-text-secondary">
-        <span v-if="isLogin">
-          {{ i18n("need_an_account") }}
-          <button
-            class="text-blue-600 hover:underline ml-1 hover:cursor-pointer"
-            type="button"
-            @click="toggleMode"
+          <h1 class="text-4xl font-bold mb-6">
+            {{ isLogin ? i18n("login") : i18n("register") }}
+          </h1>
+          <form
+            ref="authForm"
+            class="space-y-4 w-md"
+            @submit.prevent="onSubmit"
+            @input="checkAuthFormValidity"
+            @change="checkAuthFormValidity"
           >
-            {{ i18n("register") }}
-          </button>
-        </span>
+            <TextInputBox
+              v-if="!isLogin"
+              id="username"
+              v-model="username"
+              :label="i18n('username')"
+              type="text"
+              :placeholder="i18n('username_placeholder')"
+              :minlength="2"
+              :maxlength="32"
+              :required="true"
+            />
+            <TextInputBox
+              id="email"
+              v-model="email"
+              :label="i18n('email')"
+              type="email"
+              :placeholder="i18n('email_placeholder')"
+              :required="true"
+              :autofocus="true"
+            />
+            <TextInputBox
+              id="password"
+              v-model="password"
+              :label="i18n('password')"
+              type="password"
+              :placeholder="i18n('password_placeholder')"
+              :minlength="6"
+              :maxlength="64"
+              :required="true"
+            />
+            <button
+              type="submit"
+              :disabled="!canSubmit"
+              class="w-full bg-accent text-text-primary text-xl font-bold py-2 rounded-sm hover:bg-accent/80 disabled:opacity-50 hover:cursor-pointer disabled:hover:cursor-not-allowed transition-all duration-200"
+            >
+              {{ isLogin ? isLoading ? i18n("login_loading") : i18n("login") : isLoading ? i18n("register_loading") : i18n("register") }}
+            </button>
 
-        <span v-else>
-          {{ i18n("have_an_account") }}
-          <button
-            class="text-blue-600 hover:underline ml-1 hover:cursor-pointer"
-            type="button"
-            @click="toggleMode"
-          >
-            {{ i18n("login") }}
-          </button>
-        </span>
-      </p>
-    </div>
+            <p
+              v-if="errorMessage"
+              class="text-red-500 -mt-2 text-sm"
+            >
+              {{ errorMessage }}
+            </p>
+          </form>
+
+          <p class="mt-4 text-sm text-text-secondary">
+            <span v-if="isLogin">
+              {{ i18n("need_an_account") }}
+              <button
+                class="text-blue-600 hover:underline ml-1 hover:cursor-pointer"
+                type="button"
+                @click="toggleMode"
+              >
+                {{ i18n("register") }}
+              </button>
+            </span>
+
+            <span v-else>
+              {{ i18n("have_an_account") }}
+              <button
+                class="text-blue-600 hover:underline ml-1 hover:cursor-pointer"
+                type="button"
+                @click="toggleMode"
+              >
+                {{ i18n("login") }}
+              </button>
+            </span>
+          </p>
+        </div>
+      </div>
+    </Transition>
   </div>
 </template>

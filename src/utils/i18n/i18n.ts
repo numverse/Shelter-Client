d40d3c45970706957beb2559cd156311dcb9e5bf
realm2100 <kur2100a@gmail.com>
2025-12-20 00:43:13 +0900

    add error checking to i18n

--- src/utils/i18n/i18n.ts
+++ src/utils/i18n/i18n.ts
@@ -3,6 +3,16 @@ import { locales, strings } from "./localeStrings";
 const defaultLocale = "en-US";
 
 function i18n<C extends keyof typeof strings>(category: C, key: keyof typeof strings[C], locale: keyof typeof locales = defaultLocale): string {
+  const bigCategory = strings[category];
+  if (!bigCategory) {
+    console.warn(`i18n: Category "${category}" not found.`);
+    return "";
+  }
+  const smallCategory = bigCategory[key];
+  if (!smallCategory) {
+    console.warn(`i18n: Key "${String(key)}" not found in category "${category}".`);
+    return "";
+  }
   const entry = strings[category][key] as Record<keyof typeof locales, string>;
   return entry[locale] ?? entry[defaultLocale];
 }

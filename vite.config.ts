b11df412760edb15b1ac97a9ee8f1ce8965a815e
realm2100 <kur2100a@gmail.com>
2025-12-20 01:15:00 +0900

    move assets to public folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  publicDir: "src/public",
   build: {
     outDir: "build",
     emptyOutDir: true,

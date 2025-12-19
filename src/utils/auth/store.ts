100a0c76f8bb7f0382fb8fd854b844a326c93f47
realm2100 <kur2100a@gmail.com>
2025-12-19 17:49:09 +0900

    skip auth page when already authed

--- src/utils/auth/store.ts
+++ src/utils/auth/store.ts
@@ -1,7 +1,21 @@
+import { getCurrentUser } from "../api/users/getCurrentUser";
+
 let isAuthed = false;
+
+async function checkAuthed(): Promise<boolean> {
+  const res = await getCurrentUser();
+  if (!res.ok) {
+    if (res.code === "401") {
+      isAuthed = false;
+    }
+  } else {
+    isAuthed = true;
+  }
+  return isAuthed;
+}
 
 function setAuthed(value: boolean) {
   isAuthed = value;
 }
 
-export { isAuthed, setAuthed };
+export { isAuthed, checkAuthed, setAuthed };

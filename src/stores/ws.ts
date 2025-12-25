0dc4e4086a281f2ea66c761661a488566adf31b7
realm2100 <kur2100a@gmail.com>
2025-12-25 23:54:54 +0900

    change wsStore to use authStore

--- src/stores/ws.ts
+++ src/stores/ws.ts
@@ -1,21 +1,21 @@
 import { refreshTokens } from "../utils/api/auth/refreshTokens";
 import { createWebSocket } from "../utils/ws/base";
-import { setAuthed } from "./auth";
+import { authStore } from "./auth";
 
 const ws = createWebSocket("wss://shelter.zero624.dev/gateway");
 
 ws.on("open", () => {
-  setAuthed(true);
+  authStore.authed = true;
 });
 
 ws.on("close", async (evt) => {
   if (evt.reason === "AUTHENTICATION_REQUIRED") {
     const res = await refreshTokens();
     if (res.ok) {
-      setAuthed(true);
+      authStore.authed = true;
       ws.reconnect();
     } else {
-      setAuthed(false);
+      authStore.authed = false;
     }
   }
 });

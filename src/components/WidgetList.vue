84e0d97217d6abb623fdea39f0ee954d29b0ce3e
realm2100 <kur2100a@gmail.com>
2025-12-26 00:06:48 +0900

    combine seperate dm and chat buttons into one toggle button

--- src/components/WidgetList.vue
+++ src/components/WidgetList.vue
@@ -2,24 +2,17 @@
 import { CloudIcon, DollarSignIcon, MessageCircleIcon, MessagesSquareIcon, PackageIcon } from "lucide-vue-next";
 import WidgetItem from "./WidgetItem.vue";
 
-import { useRouter } from "vue-router";
-const router = useRouter();
+import { stateStore } from "../stores/state";
 </script>
 
 <template>
   <aside class="bg-bg1 p-3 border-r border-bg3 h-full">
     <ul class="flex flex-col gap-3">
       <WidgetItem
-        :icon="MessageCircleIcon"
-        short-name="DM"
-        label="Direct Messages"
-        @click="router.push('/channels/@me')"
-      />
-      <WidgetItem
-        :icon="MessagesSquareIcon"
-        short-name="CHAT"
-        label="Chat"
-        @click="router.push('/channels')"
+        :icon="stateStore.chatMode.value === 'chat' ? MessagesSquareIcon : MessageCircleIcon"
+        :short-name="stateStore.chatMode.value === 'chat' ? 'DM' : 'CHAT'"
+        :label="stateStore.chatMode.value === 'chat' ? 'Direct Messages' : 'Chat'"
+        @click="stateStore.toggleChatMode"
       />
       <li class="mx-2 my-1 border-b border-bg3" />
       <!-- example widgets for now, will be dynamic later -->

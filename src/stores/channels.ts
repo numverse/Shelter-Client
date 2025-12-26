a11bd709bce8a869cc0544b040040316e0ebfd1b
realm2100 <kur2100a@gmail.com>
2025-12-26 21:19:36 +0900

    merge dmstore into channelsstore

--- src/stores/channels.ts
+++ src/stores/channels.ts
@@ -6,6 +6,8 @@ import { Channel } from "../utils/api/types";
 const channelsStore = {
   channels: ref<Channel[]>([]),
   currentChannel: ref<Channel | null>(null),
+  dms: ref<string[]>([]),
+  currentDM: ref<string | null>(null),
   getChannel: function (id: string) {
     return channelsStore.channels.value.find((ch) => ch.id === id) ?? null;
   },
--- src/stores/dm.ts
+++
@@ -1,9 +0,0 @@
-// NOT IMPLEMENTED IN THE API YET
-import { ref } from "vue";
-
-const dmStore = {
-  dms: ref<string[]>([]),
-  currentDM: ref<string | null>(null),
-};
-
-export { dmStore };

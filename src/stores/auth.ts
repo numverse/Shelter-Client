9695c5139b29124037d17f3db5bedc99fe423ad6
realm2100 <kur2100a@gmail.com>
2025-12-27 15:16:04 +0900

    add currentUser to authStore

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,8 +1,12 @@
+import { getCurrentUser } from "../utils/api/currentUser/getCurrentUser";
 import { refreshTokens } from "../utils/api/auth/refreshTokens";
+import { CurrentUser } from "../utils/api/types";
+import { ref } from "vue";
 
 const authStore = {
   authed: false,
   refreshTimestamp: 0,
+  currentUser: ref<CurrentUser | null>(null),
   checkAuthed: async function (): Promise<boolean> {
     const res = await refreshTokens();
     if (!res.ok) {
@@ -23,6 +27,12 @@ const authStore = {
       this.authed = false;
     }
     return this.authed;
+  },
+  fetchCurrentUser: async function () {
+    const currentUser = await getCurrentUser();
+    if (currentUser.ok) {
+      this.currentUser.value = currentUser;
+    }
   },
 };
 
